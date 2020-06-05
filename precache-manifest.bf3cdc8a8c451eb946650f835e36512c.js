694f13a93d6a59b582b84ebe664ca066fa3f18e1
cody clark <codyclark2119@gmail.com>
2020-06-05 01:23:55 -0500

    Updates

---
+++ precache-manifest.bf3cdc8a8c451eb946650f835e36512c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfa66e66983ec6c7f66af2039c6c84b",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "968e709b633623c9e043",
+    "url": "/react-todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "07de51abd4ef48a5a7ae",
+    "url": "/react-todo/static/css/main.9a20c630.chunk.css"
+  },
+  {
+    "revision": "968e709b633623c9e043",
+    "url": "/react-todo/static/js/2.54109967.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-todo/static/js/2.54109967.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07de51abd4ef48a5a7ae",
+    "url": "/react-todo/static/js/main.ab6f40de.chunk.js"
+  },
+  {
+    "revision": "6faef63a301f4c382743",
+    "url": "/react-todo/static/js/runtime-main.52e4924a.js"
+  }
+]);
